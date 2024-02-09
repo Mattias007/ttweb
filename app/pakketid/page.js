4ad7531f34ccc2afe93a4a817ba8cb6867601695
AvoMaasik <119441593+AvoMaasik@users.noreply.github.com>
2024-02-09 15:50:00 +0200

    Update page.js

--- app/pakketid/page.js
+++ app/pakketid/page.js
@@ -1,13 +1,13 @@
 import Card from "@/components/card";
 import HeroImg from "@/components/heroimg";
 
-export default function Pakketid() {
+export default function Paketid() {
     return (
         <div className="max-w-full min-h-screen">
         <HeroImg />
         <div className="p-2">
 
-        <h1 className="text-lg text-center">Pakketid</h1>
+        <h1 className="text-lg text-center">Paketid</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 grid-flow-row">
 
           <Card title="Saaremaa Kortermajadele" conntent="Kuni 200Mbit/s." conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
