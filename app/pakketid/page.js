cdf029b2ae9bebed932dcdd48c36a1189b83bccd
AvoMaasik <119441593+AvoMaasik@users.noreply.github.com>
2024-02-10 13:16:15 +0200

    Update page.js

--- app/pakketid/page.js
+++ app/pakketid/page.js
@@ -10,11 +10,13 @@ export default function Paketid() {
         <h1 className="text-lg text-center">Paketid</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 grid-flow-row">
 
-          <Card title="Saaremaa Kortermajadele" conntent="Kuni 200Mbit/s." conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
-          <Card title="Saaremaa Kortermajadele" conntent="Kuni 100Mbit/s." conntent1="Üles/Alla Hind: 18€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
-          <Card title="Raadjointernet" conntent="Kuni 32Mbit/s Alla*  //  8Mbit/s Üles* " conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
-          <Card title="Raadjointernet" conntent="Kuni 16Mbit/s Alla*  //  4Mbit/s Üles* " conntent1="Üles/Alla Hind: 29€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
-          <Card title="Raadjointernet" conntent="Kuni 12Mbit/s Alla*  //  2Mbit/s Üles* " conntent1="Üles/Alla Hind: 26€/Kuus" conntent2="TV + Lisateenused" path="/pakketid/telli" butname="Telli Pakket"></Card>
+          <Card title="Kortermaja Internet" conntent="Kiirusega kuni 200Mbit/s." conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/paketid/telli" butname="Telli Pakett"></Card>
+          <Card title="Kortermaja Internet" conntent="Kiirusega kuni 100Mbit/s." conntent1="Üles/Alla Hind: 22€/Kuus" conntent2="TV + Lisateenused" path="/paketid/telli" butname="Telli Pakett"></Card>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 grid-flow-row">
+          <Card title="Raadio Internet" conntent="Kuni 32Mbit/s Alla*  //  8Mbit/s Üles* " conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/paketid/telli" butname="Telli Pakett"></Card>
+          <Card title="Raadio Internet" conntent="Kuni 16Mbit/s Alla*  //  4Mbit/s Üles* " conntent1="Üles/Alla Hind: 29€/Kuus" conntent2="TV + Lisateenused" path="/paketid/telli" butname="Telli Pakett"></Card>
+          <Card title="Raadio Internet" conntent="Kuni 12Mbit/s Alla*  //  2Mbit/s Üles* " conntent1="Üles/Alla Hind: 26€/Kuus" conntent2="TV + Lisateenused" path="/paketid/telli" butname="Telli Pakett"></Card>
         </div>
         </div>
         </div>
