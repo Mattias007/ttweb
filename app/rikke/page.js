bfbe8fe1021fd2cb39ce97a28b6252854447b5cd
AvoMaasik <119441593+AvoMaasik@users.noreply.github.com>
2024-02-10 13:19:39 +0200

    Update page.js

--- app/rikke/page.js
+++ app/rikke/page.js
@@ -14,7 +14,7 @@ export default function Rikke() {
           />
 
           <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="sm:text-lg text-base mb font-bold text-white mb-4">Rikke Teatamise Telefon :</h2>
+            <h2 className="sm:text-lg text-base mb font-bold text-white mb-4">Kontakt Telefon :</h2>
             <p className=" text-white sm:text-base text-xs mb-4">45 20 200 ja 50 97 415</p>
             <p className=" text-white sm:text-base text-xs mb-4">Või kui võimalik, siis e-posti aadressile: info@tt.ee</p>
           </div>
