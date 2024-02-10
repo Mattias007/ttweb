f61d38735f8941ab032c9985fdf413ddabb769f3
AvoMaasik <119441593+AvoMaasik@users.noreply.github.com>
2024-02-10 13:05:04 +0200

    Update page.js
    teksti ja hindade muudatused

--- app/page.js
+++ app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
         <h1 className="text-lg text-center">Internet</h1>
         <div className="flex justify-center flex-wrap items-center gap-5 p-5 flex-grow">
 
-          <Card title="Kortermaja Internet" conntent="Kuni 200Mbit/s." conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/" butname="Vaata Lähemalt"></Card>
-          <Card title="Raadio Internet" conntent="Kuni 8-32Mbit/s Alla*  //  2-8Mbit/s Üles* " conntent1="Üles/Alla Hind: 36€/Kuus" conntent2="TV + Lisateenused" path="/" butname="Vaata Lähemalt"></Card>
+          <Card title="Kortermaja Internet" conntent="Kuni 200Mbit/s." conntent1="Hind alates : 24€/Kuus" conntent2="Internet ja IPTV" path="/" butname="Vaata Lähemalt"></Card>
+          <Card title="Raadio Internet" conntent="Kuni 50Mbit/s." conntent1="Hind alates: 24€/Kuus" conntent2="Internet ja IPTV" path="/" butname="Vaata Lähemalt"></Card>
         
         </div>
         </div>
@@ -29,9 +29,9 @@ export default function Home() {
           />
 
           <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="sm:text-lg text-base mb font-bold text-white mb-4">Rikke Teatamise Telefon :</h2>
+            <h2 className="sm:text-lg text-base mb font-bold text-white mb-4">Kontakt Telefon :</h2>
             <p className=" text-white sm:text-base text-xs mb-4">45 20 200 ja 50 97 415</p>
-            <Link href="/rikke" className="bg-gradient-to-r from-blue-950 from-50% to-indigo-900 text-xs sm:text-xl text-white px-4 py-2 hover:text-cyan-100 rounded-xl">Teate Rikkest</Link>
+            <Link href="/rikke" className="bg-gradient-to-r from-blue-950 from-50% to-indigo-900 text-xs sm:text-xl text-white px-4 py-2 hover:text-cyan-100 rounded-xl">Võta ühendust</Link>
           </div>
         </div>
 
